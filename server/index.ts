ccb5d8c03c19873282df446f4e5f245baaf2ab81
Sahil Ansari Mondal <sahilansarimondal@gmail.com>
2023-09-19 12:48:49 +0530

    my own packege added

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,3 @@
-
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
@@ -14,7 +13,12 @@ app.use("/auth", authRoutes);
 app.use("/todo", todoRoutes);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+   console.log(`Example app listening at http://localhost:${port}`);
+});
 
-mongoose.connect('mongodb://localhost:27017/courses', { dbName: "courses" });
+mongoose
+   .connect(
+      "mongodb+srv://sahilansarimondal:sahil1234@sahildb.jqkiszf.mongodb.net/TodoApp"
+   )
+   .then(() => console.log("MongoDB is Connected"))
+   .catch((err) => console.log(err));
